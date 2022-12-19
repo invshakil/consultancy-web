9f58221fa4c2bb7d6dcd80833c9da10e322781f8
SeaHawks <tanvirrezaanik@gmail.com>
2022-12-20 04:08:58 +0600

    logo icons  updated

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width,initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <link rel="icon" href="/favicon.png">
+    <link rel="icon" href="{{ asset('assets/images/favicon.png') }}">
 
     <!-- Styles -->
     <link rel="stylesheet" href="{{ mix('/css/admin.css') }}">
